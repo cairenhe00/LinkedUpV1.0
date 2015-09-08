9443b33377bfcf00d8cf97fcd8bd7a14c90226bd
cairenhe00 <158164040@qq.com>
2015-09-08 14:31:49 +0800

    2015-9-8 蔡人和 Item的import包修改了下

--- src/cn/cairenhe/model/Item.java
+++ src/cn/cairenhe/model/Item.java
@@ -1,14 +1,14 @@
 package cn.cairenhe.model;
 
-import java.awt.*;
-import java.util.*;
+import java.awt.Image;
+import java.util.List;
 
 /**
  * Created by cairenhe on 2015/9/8.
  */
 public class Item extends AbstractItem {
 
-    public Item(int locationX, int locationY, int width, int height, java.util.List<Image> pictures, int currentPictureIndex, boolean visitable, boolean enable, int type, int state, int arrayLocationX, int arrayLocationY) {
+    public Item(int locationX, int locationY, int width, int height, List<Image> pictures, int currentPictureIndex, boolean visitable, boolean enable, int type, int state, int arrayLocationX, int arrayLocationY) {
         super(locationX, locationY, width, height, pictures, currentPictureIndex, visitable, enable, type, state, arrayLocationX, arrayLocationY);
     }
 }
